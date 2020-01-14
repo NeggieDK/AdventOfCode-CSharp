820f0206973c4ef418920f8d6fdc7a3e0dae635c
aaronDK <aaronDK@CI00046723>
2020-01-14 15:38:45 +0100

    Day19: Part2 clean up :)

--- AdventOfCode-CSharp/Day19.cs
+++ AdventOfCode-CSharp/Day19.cs
@@ -13,7 +13,7 @@ namespace AdventOfCode_CSharp
                 .ReadAllText(
                     "C:\\Users\\aarondk\\source\\repos\\AdventOfCode-CSharp\\AdventOfCode-CSharp\\Resources\\Day19.txt")
                 .Split(",").Select(long.Parse).ToList();
-            var scannedItems = new Dictionary<string, int>();
+            var affectedPoints = 0;
             for (var y = 0; y < 50; y++)
             {
                 for (var x = 0; x < 50; x++)
@@ -25,51 +25,32 @@ namespace AdventOfCode_CSharp
                     intComputer.Input.Enqueue(x);
                     intComputer.Input.Enqueue(y);
                     intComputer.Start();
-                    var output = intComputer.DumpOutput().Item1;
-                    scannedItems[$"{x},{y}"] = output;
-                    Console.Write(output == 1 ? "#" : ".");
+                    if (intComputer.DumpOutput().Item1 == 1)
+                        affectedPoints++;
                 }
-
-                Console.WriteLine();
             }
 
-            var found = false;
-            var yLine = 0;
-            while (!found)
+            Console.WriteLine($"Amount of affected points = {affectedPoints}");
+
+            var yLine = 100;
+            var xOffset = 0;
+            while (true)
             {
-                var quantityHorizontal = QuantityAdjacentSquaresHorizontal(yLine, intCodes.Select(i => i).ToList());
-                if (quantityHorizontal.Amount >= 100)
+                var point = GetLeftMostPointInRow(yLine, xOffset, intCodes.Select(i => i).ToList());
+                if (DoesPointExist(point + 99, yLine - 99, intCodes.Select(i => i).ToList()))
                 {
-                    var adjacentWith100Depth = 0;
-                    for (var xLine = quantityHorizontal.EndX; xLine >= quantityHorizontal.StartX; xLine--)
-                    {
-                        var quantityVertical = QuantityAdjacentSquaresVertical(xLine, yLine, intCodes.Select(i => i).ToList());
-                        if (quantityVertical.Amount < 100) 
-                            break;
-                        adjacentWith100Depth++;
-                        if (adjacentWith100Depth >= 100)
-                        {
-                            Console.WriteLine($"Part2: {xLine*1000+yLine}");
-                            break;
-                            found = true;
-                        }
-                    }
+                    Console.WriteLine($"Part2: {point*10000+(yLine-99)}");
+                    break;
                 }
-
                 yLine++;
+                xOffset = point;
             }
-            //Correct answer = 18651593 (calculated with very inefficient slow code :()
         }
 
-        public static HorizontalLine QuantityAdjacentSquaresHorizontal(int y, List<long> intCodes)
+        public static int GetLeftMostPointInRow(int y, int xOffset, List<long> intCodes)
         {
-            var done = false;
-            var x = 0;
-            var previousOutput = 0;
-            var startX = 0;
-            var endX = 0;
-                
-            while (!done)
+            var x = xOffset;
+            while (true)
             {
                 var intComputer = new IntComputer
                 {
@@ -78,97 +59,24 @@ namespace AdventOfCode_CSharp
                 intComputer.Input.Enqueue(x);
                 intComputer.Input.Enqueue(y);
                 intComputer.Start();
+                
                 var output = intComputer.DumpOutput().Item1;
-                if (previousOutput == 1 && output == 0)
-                {
-                    endX = x - 1;
-                    return new HorizontalLine
-                    {
-                        EndX = endX,
-                        StartX = startX,
-                        Amount = endX - startX + 1,
-                        Y = y
-                    };
-                }
-                else if (previousOutput == 0 && output == 1)
-                {
-                    startX = x ;
-                }
-
-                if (x == 5000) done = true;
-                previousOutput = output;
+                if (output == 1) return x;
                 x++;
             }
-            return new HorizontalLine
-            {
-                Y = 0,
-                EndX = 0,
-                StartX = 0,
-                Amount = 0
-            };
         }
 
-        public static VerticalLine QuantityAdjacentSquaresVertical(int x, int yOffset, List<long> intCodes)
+        public static bool DoesPointExist(int x, int y, List<long> intCodes)
         {
-            var done = false;
-            var y = yOffset;
-            var previousOutput = 0;
-            var startY = 0;
-            var endY = 0;
-                
-            while (!done)
+            var intComputer = new IntComputer
             {
-                var intComputer = new IntComputer
-                {
-                    IntCodes = intCodes.Select(i => i).ToList()
-                };
-                intComputer.Input.Enqueue(x);
-                intComputer.Input.Enqueue(y);
-                intComputer.Start();
-                var output = intComputer.DumpOutput().Item1;
-                if (previousOutput == 1 && output == 0)
-                {
-                    endY = y - 1;
-                    return new VerticalLine
-                    {
-                        EndY = endY,
-                        StartY = startY,
-                        Amount = endY - startY + 1,
-                        X = x
-                    };
-                }
-                else if (previousOutput == 0 && output == 1)
-                {
-                    startY = y;
-                }
-
-                if (y == 5000) done = true;
-                previousOutput = output;
-                y++;
-            }
-            return new VerticalLine
-            {
-                X = 0,
-                EndY = 0,
-                StartY = 0,
-                Amount = 0
+                IntCodes = intCodes.Select(i => i).ToList()
             };
+            intComputer.Input.Enqueue(x);
+            intComputer.Input.Enqueue(y);
+            intComputer.Start();
+            var output = intComputer.DumpOutput().Item1;
+            return output == 1;
         }
-    }
-
-    class HorizontalLine
-    {
-        public int StartX { get; set;}
-        public int EndX { get; set; }
-        public int Amount { get; set; }
-        public int Y { get; set; }
-    }
-
-    class VerticalLine
-    {
-        public int StartY { get; set;}
-        public int EndY { get; set; }
-        public int Amount { get; set; }
-        public int X { get; set; }
     }
 }
