22871d31034a2cfeaf9e53e37f744ea11ed69654
Neggie <aaron.dekoninck@gmail.com>
2020-01-23 16:28:39 +0100

    Day21: Part2 preparations

--- AdventOfCode-CSharp/Day21.cs
+++ AdventOfCode-CSharp/Day21.cs
@@ -15,6 +15,9 @@ namespace AdventOfCode_CSharp
             var instructions = System.IO.File
                 .ReadAllText(
                     "C:\\Users\\aarondk\\source\\repos\\AdventOfCode-CSharp\\AdventOfCode-CSharp\\Resources\\Day21Instruction.txt");
+            var instructions2 = System.IO.File
+                .ReadAllText(
+                    "C:\\Users\\aarondk\\source\\repos\\AdventOfCode-CSharp\\AdventOfCode-CSharp\\Resources\\Day21InstructionP2.txt");
             var intComputer = new IntComputer
             {
                 IntCodes = intCodes.Select(i => i).ToList()
@@ -31,6 +34,22 @@ namespace AdventOfCode_CSharp
                 Console.Write((char)el);
             }
             Console.WriteLine($"Part1: amount of hull damage {output.Last()}");
+            var intComputer2 = new IntComputer
+            {
+                IntCodes = intCodes.Select(i => i).ToList()
+            };
+            foreach (var el in instructions2)
+            {
+                if ( el == 13) continue;
+                intComputer2.Input.Enqueue(el);
+            }
+            intComputer2.Start();
+            var output2 = intComputer2.DumpFullOutput();
+            foreach (var el in output2)
+            {
+                Console.Write((char)el);
+            }
+            Console.WriteLine($"Part2: amount of hull damage {output2.Last()}");
         }
     }
 }
